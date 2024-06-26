b31b5f340d796a4d85d400c31934821bd643a3c2
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-06-26 10:50:58 +0500

    commited

--- backend/libs/share/src/entities/user.entity.ts
+++ backend/libs/share/src/entities/user.entity.ts
@@ -8,39 +8,51 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { Role } from './role.entity';
+import { IsBoolean, IsDate, IsEmail, IsInt, IsString } from 'class-validator';
 
 @Entity({ name: 'users' })
 export class User {
+  @IsInt()
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @IsString()
   @Column()
   fullname: string;
 
+  @IsEmail()
   @Column({ unique: true })
   email: string;
 
+  @IsString()
   @Column()
   phone: string;
 
+  @IsDate()
   @Column()
   birth: Date;
 
+  @IsInt()
   @Column({ unique: true })
   profile: number;
 
+  @IsBoolean()
   @Column({ default: false })
   verifed: boolean;
 
+  @IsString()
   @Column({ name: 'password_hash', nullable: true })
   passwordHash: string;
 
+  @IsString()
   @Column({ name: 'refresh_token', nullable: true })
   refreshToken: string;
 
+  @IsDate()
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @IsDate()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 
